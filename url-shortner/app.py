95dffd844533ef62edb12fef7c3916565b64cdc7
shrey08 <shreyarora1967@gmail.com>
2020-06-22 19:41:16 +0530

    Modified

--- url-shortner/app.py
+++ url-shortner/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect, url_for, flash
+from flask import Flask,render_template,request,redirect, url_for, flash, abort
 import json
 import os.path
 from werkzeug.utils import secure_filename
@@ -33,7 +33,7 @@ def your_url():
 		else:
 			f = request.files['file']
 			full_name = request.form['code'] + secure_filename(f.filename)
-			f.save('D:/Flask-project/url-shortner' + full_name)
+			f.save('D:/Flask-project/url-shortner/static/user_files/' + full_name)
 			urls[request.form['code']]={'file':full_name}
 
 		with open('urls.json','w') as url_file:
@@ -43,3 +43,19 @@ def your_url():
 	
 	else:
 		return redirect(url_for('home'))
+
+@app.route('/<string:code>')
+def redirect_to_url(code):
+	if os.path.exists('urls.json'):
+		with open('urls.json') as urls_file:
+			urls=json.load(urls_file)
+			if code in urls.keys():
+				if 'url' in urls[code].keys():
+					return redirect(urls[code]['url'])
+				else:
+					return redirect(url_for('static',filename='user_files/' + urls[code]['file']))
+	return abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+	return render_template('page_not_found.html'), 404
