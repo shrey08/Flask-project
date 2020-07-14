c763d5eed5208754531b24bcc1b999cc47b6d04f
shrey08 <shreyarora1967@gmail.com>
2020-07-14 22:33:46 +0530

    login-route-interact-with-database

--- enrollment/application/routes.py
+++ enrollment/application/routes.py
@@ -1,5 +1,5 @@
 from application import app, db
-from flask import render_template, request, json, Response, redirect, flash
+from flask import render_template, request, json, Response, redirect, flash, url_for
 from application.models import User, Course, Enrollment
 from application.forms import LoginForm, RegisterForm
 
@@ -16,8 +16,12 @@ def index():
 def login():
 	form = LoginForm()
 	if form.validate_on_submit():
-		if request.form.get("email") == "test@uta.com":
-			flash("You are successfully logged in!", "success")
+		email=form.email.data
+		password=form.password.data
+		
+		user=User.objects(email=email).first()
+		if user and user.password==password:
+			flash(f"{user.first_name},You are successfully logged in!", "success")
 			return redirect("/index")
 		else:
 			flash("Sorry! Something went wrong.", "danger")
