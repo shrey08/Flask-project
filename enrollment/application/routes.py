2a3bc7635e859b920746723a858036d5667b529a
shrey08 <shreyarora1967@gmail.com>
2020-07-16 22:33:15 +0530

    CRUD

--- enrollment/application/routes.py
+++ enrollment/application/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, request, json, jsonify, Response, redirect, f
 from application.models import User, Course, Enrollment
 from application.forms import LoginForm, RegisterForm
 from flask_restplus import Resource 
+from application.course_list import course_list
 
 courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
 
@@ -10,13 +11,37 @@ courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP",
 
 @api.route('/api','/api/')
 class GetAndPost(Resource):
+	
+	#Get ALL
 	def get(self):
 		return jsonify(User.objects.all())
+	
+	#POST
+	def post(self):
+		data=api.payload
+		user = User(user_id=data['user_id'], email=data['email'],password=data['password'],first_name=data['first_name'],last_name=data['last_name'])
+		user.set_password(data['password'])
+		user.save()
+		return jsonify(User.objects(user_id=data['user_id']))
+
 
 @api.route('/api/<idx>')
 class GetUpdateDelete(Resource):
+	
+	#Get ONE
 	def get(self,idx):
 		return jsonify(User.objects(user_id=idx))
+
+	#PUT
+	def put(self,idx):
+		data=api.payload
+		User.objects(user_id=idx).update(**data)
+		return jsonify(User.objects(user_id=idx))
+
+	#DELETE
+	def delete(self,idx):
+		User.objects(user_id=idx).delete()
+		return jsonify("User is deleted!")
 
 
 ##################################################################
@@ -98,54 +123,19 @@ def enrollment():
 			enroll=Enrollment(user_id=user_id,courseID=courseID)
 			enroll.save()
 			flash(f"You are enrolled in {courseTitle}!","success")
-	classes= list(User.objects.aggregate(*[
-			{
-				'$lookup': {
-					'from': 'enrollment', 
-					'localField': 'user_id', 
-					'foreignField': 'user_id', 
-					'as': 'r1'
-				}
-			}, {
-				'$unwind': {
-					'path': '$r1', 
-					'includeArrayIndex': 'r1_id', 
-					'preserveNullAndEmptyArrays': False
-				}
-			}, {
-				'$lookup': {
-					'from': 'course', 
-					'localField': 'r1.courseID', 
-					'foreignField': 'courseID', 
-					'as': 'r2'
-				}
-			}, {
-				'$unwind': {
-					'path': '$r2', 
-					'preserveNullAndEmptyArrays': False
-				}
-			}, {
-				'$match': {
-					'user_id': user_id
-				}
-			}, {
-				'$sort': {
-					'courseID': 1
-				}
-			}
-		]))
-
+	
+	courses=course_list()
 	return render_template("enrollment.html", enrollment=True, title="Enrollment", classes=classes)
 
-@app.route('/api/')
-@app.route('/api/<idx>')
-def api(idx=None):
-	if idx==None:
-		jData=courseData
-	else:
-		jData=courseData[int(idx)]
+# @app.route('/api/')
+# @app.route('/api/<idx>')
+# def api(idx=None):
+# 	if idx==None:
+# 		jData=courseData
+# 	else:
+# 		jData=courseData[int(idx)]
 
-	return Response(json.dumps(jData), mimetype='application/json')
+# 	return Response(json.dumps(jData), mimetype='application/json')
 
 @app.route('/user')
 def user():
